c0228482ffdda08f9456759b6232bce8e7c1236c
Gus <aengus.crowley@gmail.com>
2016-10-18 14:58:55 -0700

    added server written in typescript

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+    //res.send( THE HOMIES HOMEPAGE);
+});
+app.listen(3000);
---
+++ server.ts
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+	//res.send( THE HOMIES HOMEPAGE);
+});
+
+app.listen(3000);
