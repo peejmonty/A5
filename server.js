54a36b87667fdfba1503ba2f1da5d00fd143124f
Gus <aengus.crowley@gmail.com>
2016-10-23 17:19:36 -0700

    added public files to server and added first page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname));
 app.get('/', function (req, res) {
     //res.send( THE HOMIES HOMEPAGE);
 });
--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname));
+
 app.get('/',(req,res)=>{
 	//res.send( THE HOMIES HOMEPAGE);
 });
